578bee770667c5bb0dcaecd588d5f72dd13712c5
serpac <sergioppastrana@gmail.com>
2021-05-22 13:47:25 +0200

    Correccion testing raiz

--- calculator.py
+++ calculator.py
@@ -20,5 +20,20 @@ class Calculator():
         return result
     
     def raiz(operand1):
+
+
+        x = Calculator.division(operand1, 2)
+
+        for i in range(15):
+            if Calculator.multiplicacion(x,x) == operand1:
+                return x
+              
+            else:
+                division = Calculator.division(operand1, x)
+                suma = Calculator.suma(x, division)
+                lastdivision = Calculator.division(suma, 2)
+                x = lastdivision        
+    
+        return x
         
-        return 0
+        # return 0
